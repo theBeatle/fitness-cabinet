c1e739b5d8fe0c3504e8fc1240e1530b046a1320
Vitaliy Alexandrovich <Dobrota2017@outlook.com>
2019-04-17 00:24:40 +0300

    FirstListCommit

---
+++ FitnessApp/ClientApp/src/app/list/list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-component',
+  templateUrl: './list.component.html'
+})
+
+export class ListComponent {
+  title = 'app';
+}
