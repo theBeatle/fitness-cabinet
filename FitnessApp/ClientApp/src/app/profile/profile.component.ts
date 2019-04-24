4aafdc9ae8275fb9a9ebb29b951b90326a399042
Iskandirov <Iskandirov@A33COMP13>
2019-04-24 18:43:14 +0300

    Customize a simple page with material

--- FitnessApp/ClientApp/src/app/profile/profile.component.ts
+++ FitnessApp/ClientApp/src/app/profile/profile.component.ts
@@ -5,11 +5,29 @@ import { MatSidenavModule } from '@angular/material/sidenav';
   selector: 'profile-data',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
+  template: `
+
+
+  <mat-form-field class="example-full-width">
+    <input matInput placeholder="Name">
+  </mat-form-field>
+  <br />
+  <mat-form-field class="example-full-width">
+    <textarea matInput placeholder="Leave a comment"></textarea>
+  </mat-form-field>
+  <br/>
+  <mat-button-toggle-group name="fontStyle" aria-label="Font Style">
+    <mat-button-toggle (click)="onClickMe()">Click me!</mat-button-toggle>
+  </mat-button-toggle-group>
+    {{clickMessage}}`
 })
 
 
 export class ProfileComponent {
-  showFiller = false;
-  title = 'app';
+  clickMessage = '';
+
+  onClickMe() {
+    this.clickMessage = 'You are my hero!';
+  }
 }
 
