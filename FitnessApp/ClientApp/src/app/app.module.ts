6ac506f2372a95feef6b571a32a50fc1a3961e76
ut_mat <ut_mat@A33COMP72>
2019-04-25 20:39:27 +0300

    changes added

--- FitnessApp/ClientApp/src/app/app.module.ts
+++ FitnessApp/ClientApp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { RegisterComponent } from './registration/register/register.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
+    RegisterComponent,
     FetchDataComponent
   ],
   imports: [
@@ -26,6 +28,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'registration', component: RegisterComponent },
     ])
   ],
   providers: [],
