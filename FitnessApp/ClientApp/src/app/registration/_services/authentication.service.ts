be1acd249e2c0840f6a7c28765fa5b6eaca683a4
ut_mat <ut_mat@A33COMP72>
2019-05-06 20:48:07 +0300

    environment fixed (maybe)

--- FitnessApp/ClientApp/src/app/registration/_services/authentication.service.ts
+++ FitnessApp/ClientApp/src/app/registration/_services/authentication.service.ts
@@ -1,8 +1,8 @@
-﻿import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
-import { environment } from '../../environments/environment';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class AuthenticationService {
