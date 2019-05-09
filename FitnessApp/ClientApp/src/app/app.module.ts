9629a21882c02bad0e2e8d9e42404b085cd99346
Iskandirov <Iskandirov@A33COMP13>
2019-05-09 19:55:39 +0300

    merge registration & login

--- FitnessApp/ClientApp/src/app/app.module.ts
+++ FitnessApp/ClientApp/src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; 
 import { HttpModule, XHRBackend } from '@angular/http';
 import { AuthenticateXHRBackend } from './authenticate-xhr.backend';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
-import { routing } from './app.routing';
 
 /* App Root */
 import { MaterialModule } from './material';
@@ -15,8 +12,6 @@ import { ProfileComponent } from './profile/profile.component';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatMenuModule, MatDatepickerModule } from '@angular/material';
@@ -36,13 +31,9 @@ import { ConfigService } from './shared/utils/config.service';
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     ProfileComponent,
     HeaderComponent,
-    HomeComponent      
   ],
   imports: [
     AccountModule,
@@ -51,6 +42,7 @@ import { ConfigService } from './shared/utils/config.service';
     FormsModule,
     ReactiveFormsModule,
     MatButtonModule,
+    HttpModule,
     MatMenuModule,
     MaterialModule,
     MatDatepickerModule,
@@ -66,20 +58,17 @@ import { ConfigService } from './shared/utils/config.service';
     BrowserAnimationsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'profile', component: ProfileComponent },
     ]),
     AppRoutingModule
   ],
   providers: [
-    HttpClientModule,
-    PersonService
+   
+    ConfigService, {
+      provide: XHRBackend,
+      useClass: AuthenticateXHRBackend
+    }
   ],
-  providers: [ConfigService, { 
-    provide: XHRBackend, 
-    useClass: AuthenticateXHRBackend
-  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- FitnessApp/ClientApp/src/app/header/header.component.ts
+++ FitnessApp/ClientApp/src/app/header/header.component.ts
@@ -3,6 +3,8 @@ import {Subscription} from 'rxjs/Subscription';
 
 import { UserService } from '../shared/services/user.service';
 
+import { Router } from "@angular/router";
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -14,11 +16,15 @@ export class HeaderComponent implements OnInit,OnDestroy {
   status: boolean;
  subscription:Subscription;
 
-  constructor(private userService:UserService) {     
+  constructor(private userService: UserService, private router: Router) {     
    }
 
    logout() {
      this.userService.logout();       
+  }
+
+  profile() {
+    this.router.navigate(["profile"]);
   }
 
   ngOnInit() {
